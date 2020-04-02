f0da3fdb915939073221f7338c7d1d652b61bb57
theo-poujol <theo.poujol@etu.univ-amu.fr>
2020-04-02 12:29:43 +0200

    C3a2nasm class

--- src/C3a2nasm.java
+++ src/C3a2nasm.java
@@ -1,5 +1,4 @@
-import c3a.C3a;
-import c3a.C3aVisitor;
+import c3a.*;
 import nasm.Nasm;
 import nasm.NasmOperand;
 import ts.Ts;
@@ -13,13 +12,124 @@ public class C3a2nasm implements C3aVisitor<NasmOperand> {
     private TsItemFct currentFct;
 
 
-
     public C3a2nasm(C3a c3a, Ts table) {
         this.c3a = c3a;
         this.tableGlobale = table;
         this.nasm = new Nasm(this.tableGlobale);
+        this.currentFct = this.tableGlobale.getFct("main");
     }
 
 
     public Nasm getNasm() { return this.nasm;}
+
+
+    @Override
+    public NasmOperand visit(C3aInstAdd inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstCall inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstFBegin inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInst inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstJumpIfLess inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstMult inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstRead inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstSub inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstAffect inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstDiv inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstFEnd inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstJumpIfEqual inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstJumpIfNotEqual inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstJump inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstParam inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstReturn inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstWrite inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aConstant oper) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aLabel oper) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aTemp oper) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aVar oper) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aFunction oper) {
+        return null;
+    }
 }
